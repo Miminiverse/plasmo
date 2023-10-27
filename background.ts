84b45d3fdf25759011b08c6fbbb87718ac152f47
miminiverse <minhtamphamtol@gmail.com>
2023-10-27 11:40:47 -0400

    adding testing Postman

--- background.ts
+++ background.ts
@@ -1,41 +1,15 @@
-import tldr from "wikipedia-tldr"
-
 chrome.runtime.onInstalled.addListener(function () {
   chrome.contextMenus.create({
-    title: 'Quick Wiki Lookup For "%s"',
+    title: 'Quick Translate For "%s"',
     contexts: ["selection"],
     id: "myContextMenuId"
   })
 })
 
-type WikiTldrThumbnail = {
-  source: string
-  width: number
-  height: number
-}
-
-export type WikiTldr = {
-  query: string
-  type: string
-  title: string
-  displaytitle: string
-  thumbnail: WikiTldrThumbnail
-  originalimage: WikiTldrThumbnail
-  lang: string
-  description: string
-  extract: string
-  extract_html: string
-}
-
-export type WikiMessage = {
-  type: string
-  text: WikiTldr
-}
-
 const trans = async (text) => {
   const requestBody = {
     text: text,
-    targetLanguage: "vi" // Replace with your target language
+    targetLanguage: "es" // Replace with your target language
   }
 
   const response = await fetch("http://localhost:8001/api/v1/translation", {
--- content.tsx
+++ content.tsx
@@ -1,8 +1,6 @@
 import cssText from "data-text:~/style.css"
 import { useEffect, useState } from "react"
 
-import type { WikiMessage, WikiTldr } from "~background"
-
 export const getStyle = () => {
   const style = document.createElement("style")
   style.textContent = cssText
@@ -10,29 +8,24 @@ export const getStyle = () => {
 }
 
 function IndexPopup() {
-  const [wikiTldr, setWikiTldr] = useState<WikiTldr>(null)
+  const [translate, setTranslate] = useState(null)
 
   useEffect(() => {
     chrome.runtime.onMessage.addListener(function ({ type, text }) {
-      if (type === "lookup") {
-        console.log(type, text)
-      }
+      setTranslate(text)
       return true
     })
   }, [])
 
   return (
     <div>
-      <div className="block p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+      <div className="p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
         <h1 className="mb-2 text-3xl font-bold tracking-tight text-gray-900 dark:text-white">
-          Wikipedia TLDR
+          Hi I'm your translation bot!
         </h1>
         <h2 className="mb-2 text-2xl font-bold tracking-tight text-gray-600 dark:text-white">
-          {wikiTldr && wikiTldr["title"]}
+          {translate}
         </h2>
-        <p className="font-normal text-gray-700 dark:text-gray-400">
-          {wikiTldr && wikiTldr["extract"]}
-        </p>
       </div>
     </div>
   )
